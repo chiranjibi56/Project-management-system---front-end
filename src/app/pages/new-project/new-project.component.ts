65f016ae9da87f62748ecbd31f51810e06f7f694
LAPTOP-JF164JR2\chira <chiranjibi.dangal@gmail.com>
2024-09-07 15:45:26 -0400

    Added validation logic on new porject page.If user just hover the mouse each field and did not provide the fild name, it will display the message.

--- src/app/pages/new-project/new-project.component.ts
+++ src/app/pages/new-project/new-project.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { ProjectService } from '../../services/project.service';
 import { Project } from '../../models/project';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-new-project',
   standalone: true,
-  imports: [FormsModule, ReactiveFormsModule, RouterLink],
+  imports: [FormsModule, ReactiveFormsModule, RouterLink, CommonModule],
   templateUrl: './new-project.component.html',
   styleUrl: './new-project.component.scss',
 })
@@ -15,48 +16,54 @@ export class NewProjectComponent implements OnInit {
   newProjectForm: FormGroup;
   projectId: number;
 
-
-  constructor(private formBuilder: FormBuilder,
+  constructor(
+    private formBuilder: FormBuilder,
     private projectService: ProjectService,
     private route: ActivatedRoute,
-    private router:Router
+    private router: Router
   ) {}
 
   ngOnInit(): void {
     this.newProjectForm = this.formBuilder.group({
-      projectName: [''],
-      startDate: [''],
-      endDate: [''],
-      projectManager: [''],
-      projectBudget: [''],
-      projectStatus: [''],
-      projectType: [''],
+      projectName: ['', Validators.required],
+      startDate: ['', Validators.required],
+      endDate: ['', Validators.required],
+      projectManager: ['', Validators.required],
+      projectBudget: ['', Validators.required],
+      projectStatus: ['', Validators.required],
+      projectType: ['', Validators.required],
     });
 
     this.projectId = parseInt(this.route.snapshot.paramMap.get('id'));
-    if(this.projectId){  //only for edit action
-      this.projectService.getProjectById(this.projectId).subscribe((projectDataFromDatabase:Project) =>{
-        console.log(projectDataFromDatabase)
-        this.newProjectForm.patchValue(projectDataFromDatabase);
-      })
+    if (this.projectId) {
+      //only for edit action
+      this.projectService
+        .getProjectById(this.projectId)
+        .subscribe((projectDataFromDatabase: Project) => {
+          console.log(projectDataFromDatabase);
+          this.newProjectForm.patchValue(projectDataFromDatabase);
+        });
     }
   }
 
   saveProject(): void {
     const projectDeta = this.newProjectForm.value;
 
-    if(this.projectId){
-      this.projectService.updateProject(this.projectId, projectDeta).subscribe((result:string) =>{
-        alert(result);
-        this.router.navigate(['list']);
-      })
-
-    }else{
-    this.projectService.saveProject(projectDeta).subscribe((result: string)=>{
-      alert(result)
-    })
-    //console.log(this.newProjectForm.value);
-  }
+    if (this.projectId) {
+      this.projectService
+        .updateProject(this.projectId, projectDeta)
+        .subscribe((result: string) => {
+          alert(result);
+          this.router.navigate(['list']);
+        });
+    } else {
+      this.projectService
+        .saveProject(projectDeta)
+        .subscribe((result: string) => {
+          alert(result);
+        });
+      //console.log(this.newProjectForm.value);
+    }
   }
 }
  
