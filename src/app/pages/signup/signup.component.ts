7d006a912bc12e75e40971426efaf66860a95770
LAPTOP-JF164JR2\chira <chiranjibi.dangal@gmail.com>
2024-09-09 13:34:33 -0400

    Configuring primeNg and applying toast message in login page

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -2,21 +2,33 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Router, RouterLink, RouterModule } from '@angular/router';
 import { UserService } from '../../services/user.service';
+// import { ToastModule } from 'primeng/toast';
+// import { MessageService } from 'primeng/api';
 
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [FormsModule, ReactiveFormsModule, RouterLink, RouterModule],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    RouterLink,
+    RouterModule,
+    // ToastModule,
+  ],
+  // providers: [MessageService],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss',
 })
-export class SignupComponent implements OnInit{
+export class SignupComponent implements OnInit {
   signUpForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder,
+  constructor(
+    private formBuilder: FormBuilder,
     private userService: UserService,
-  private router:Router) {}
+    private router: Router,
+    // private messageService: MessageService
+  ) {}
 
   ngOnInit(): void {
     this.signUpForm = this.formBuilder.group({
@@ -30,16 +42,15 @@ export class SignupComponent implements OnInit{
     });
   }
 
-  signUp():void{
-    
+  signUp(): void {
     const user = this.signUpForm.value;
-    this.userService.signUp(user).subscribe((result:string)=>{
-      if(result === 'success'){
+    this.userService.signUp(user).subscribe((result: string) => {
+      if (result === 'success') {
+        // this.messageService.add({severity: 'success', summary: 'success', detail: 'User created successfully!' })
         this.router.navigate(['']); // Redirecting to login page after user register
-      }else{
-        alert('Unable to register user!')
+      } else {
+        alert('Unable to register user!');
       }
-    })
-
+    });
   }
 }
